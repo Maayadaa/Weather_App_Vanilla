d28d6bd7b2e7a898b546f4627eec19b58ac85be3
Mayada <Maayadaa@users.noreply.github.com>
2023-03-20 17:02:19 +0200

    Built the app

--- src/app.js
+++ src/app.js
@@ -41,11 +41,59 @@ function showWeather(response) {
   icon.src = `https://openweathermap.org/img/wn/${newIcon}@2x.png`;
 }
 
+//fotmat date
+function formatDay(timeStamp) {
+  let date = new Date(timeStamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+//forecast function hTML
+function showForecast(response) {
+  let Forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecastRow");
+  let forecastHTML = "";
+
+  Forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+          <div class="col-2">
+            <div id="forecastDay">${formatDay(forecastDay.time)}</div>
+            <div class="row align-items-start">
+              <img
+                src="${forecastDay.condition.icon_url}"
+                alt=""
+                id="forecastIcon"
+              />
+            </div>
+            <div class="row align-items-start" id="max_min">
+              <div>
+                <span id="maxTemp">${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</span>
+                <span id="minTemp">${Math.round(
+                  forecastDay.temperature.minimum
+                )}°</span>
+              </div>
+            </div>
+          </div>
+        `;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function changeCity(event) {
   event.preventDefault();
   if (cityInput.value.trim()) {
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(showWeather);
+    //forecast
+    let apiURLForecast = `https://api.shecodes.io/weather/v1/forecast?query=${cityInput.value}&key=4f13403b55o7f4ea4b270ff5d243at5e&units=metric`;
+    axios.get(apiURLForecast).then(showForecast);
   } else {
     alert("Please enter your city!");
   }
@@ -58,10 +106,15 @@ function changeFeh(response) {
   temp.innerHTML = `${newTemp}°F`;
 }
 
+//forecast fah
+
 function changeToFeh(event) {
   event.preventDefault();
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=imperial`;
   axios.get(apiURL).then(changeFeh);
+
+  let apiURLForecast = `https://api.shecodes.io/weather/v1/forecast?query=${cityInput.value}&key=4f13403b55o7f4ea4b270ff5d243at5e&units=imperial`;
+  axios.get(apiURLForecast).then(showForecast);
 }
 
 let fehTemp = document.querySelector("#fehUnit");
